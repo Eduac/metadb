712ab40d405bff7c5b82ff70e5fe823d99e8998f
Miguel Haruki Yamaguchi <haruki.yamaguchi1989@gmail.com>
2011-07-23 12:56:14 -0400

    prepare for Node ver

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Metadb::Application.config.secret_token = 'ea1d9fc06e74a0eb29c1056e25a671c486fc21da3f16e9e749b614bb5a01240e59a800ccbc036eeac8cc309bafe3f42fa09ef8ce959419f79690cd4bcaef7784'
