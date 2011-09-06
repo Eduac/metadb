b550c9be477e8421531cc6d498cc225740f82215
Long Ho <holevietlong@gmail.com>
2011-09-05 21:14:10 -0400

    changed to port 80

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var app = express.createServer();
 var fs = require('fs');
 
 var public_path = 'public/';
-var PORT = 8080;
+var PORT = 80;
 var oneDay = 86400000;
 
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
 console.log("Server running at port " +  PORT);
-app.listen(8080);
+app.listen(PORT);
