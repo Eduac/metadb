3732cce17d3654ce6c4de381ef41d30d98604a80
Long Ho <holevietlong@gmail.com>
2011-10-12 23:13:19 -0400

    remove binaries, added views and template engine to server.js

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 var express = require('express');
 var app = express.createServer();
 var fs = require('fs');
+var MemoryStore = express.session.MemoryStore;
 
-var public_path = 'public/';
-var PORT = 80;
-var oneDay = 86400000;
+var PORT = 8081;
+app.use(express.static(__dirname + '/public', {maxAge : 86400000}));
 
-app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'MDB-L0ngH0-H4rUk!', store: new MemoryStore({ reapInterval: 60000 * 10 }) }));
+app.set("view engine", "html");
+app.register(".html", require("jqtpl").express);
+app.set('view options', { layout: false });
+
+app.get('/', function(req, res) {
+	res.render('login');
+});
+
+app.post('/login', function(req, res) {
+	console.log(req.query);
+	req.session.token=123;
+	req.session.username='bernie';
+	res.redirect('/viewer');
+});
+
+app.get('/viewer', function(req, res) {
+	console.log(req.query);
+	if (!req.session || !req.session.token || !req.session.username) {
+		res.redirect('/');
+	}
+	res.render('viewer'); 
+});
 
 console.log("Server running at port " +  PORT);
 app.listen(PORT);
